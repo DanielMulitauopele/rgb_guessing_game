4d418bf1a11488232f024ec9cfaaedc8bc9b3c93
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-05-16 14:35:58 -0600

    Adds variable names

---
+++ app.js
@@ -0,0 +1,6 @@
+let numCircles = 6;
+let colors  = [];
+let pickedColor;
+let circles = document.querySelectorAll(".circle");
+const resetButton = document.querySelector("#reset");
+const modeButtons = document.querySelectorAll(".mode");
