5dacd43fdf30994a87245f1bc9613e36e7634ae6
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-05-16 14:49:25 -0600

    Adds unique colors, adds event listeners

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 let numCircles = 6;
-let colors  = [];
+
+let colors  = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 0, 255)",
+  "rgb(255, 225, 0)",
+  "rgb(255, 0, 255)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 255, 0)"
+];
+
 let pickedColor;
 let circles = document.querySelectorAll(".circle");
 const resetButton = document.querySelector("#reset");
 const modeButtons = document.querySelectorAll(".mode");
+
+for (let i = 0; i < circles.length; i++){
+  circles[i].style.backgroundColor = colors[i]
+};
+
+for (let i = 0; i < circles.length; i++) {
+  circles[i].addEventListener('click', function() {
+    alert('option was clicked!');
+  })
+}
