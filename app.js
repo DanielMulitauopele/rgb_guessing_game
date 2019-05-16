c0b0a02442e96df34dfd276dfa90545d6ae916f9
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-05-16 15:27:06 -0600

    Adds basic js for guessing game, needs random color generator function

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-let numCircles = 6;
+const randomColor = (colors) => {
+  return colors[Math.floor(Math.random() * (numCircles - 1) + 1)]
+}
 
+const fillColors = () => {
+  for (let i = 0; i < circles.length; i++){
+    circles[i].style.backgroundColor = colors[i]
+  };
+};
+
+const runGame = () => {
+  for (let i = 0; i < circles.length; i++) {
+    circles[i].addEventListener('click', function() {
+      let clickedColor = this.style.backgroundColor;
+
+      if (clickedColor == pickedColor) {
+        alert('That is correct!');
+        fillColors();
+      } else {
+        this.style.backgroundColor = "#232323";
+        alert('That is incorrect!');
+      }
+    })
+  }
+}
+
+let numCircles = 6;
+const circles = document.querySelectorAll(".circle");
+const resetButton = document.querySelector("#reset");
+const modeButtons = document.querySelectorAll(".mode");
 let colors  = [
   "rgb(255, 0, 0)",
   "rgb(255, 0, 255)",
@@ -8,18 +36,7 @@ let colors  = [
   "rgb(0, 255, 255)",
   "rgb(0, 255, 0)"
 ];
+let pickedColor = randomColor(colors);
 
-let pickedColor;
-let circles = document.querySelectorAll(".circle");
-const resetButton = document.querySelector("#reset");
-const modeButtons = document.querySelectorAll(".mode");
-
-for (let i = 0; i < circles.length; i++){
-  circles[i].style.backgroundColor = colors[i]
-};
-
-for (let i = 0; i < circles.length; i++) {
-  circles[i].addEventListener('click', function() {
-    alert('option was clicked!');
-  })
-}
+fillColors();
+runGame();
